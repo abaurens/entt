583b5ebf93fee89b5ed368e5e08baa1a2d4634fc
abaurens <abaurens@student.42.fr>
2024-06-15 02:54:59 +0200

    Added premake5.lua file

---
+++ premake5.lua
@@ -0,0 +1,26 @@
+-- entt
+
+project "entt"
+  kind "None"
+  language "C++"
+  cppdialect "C++17"
+  systemversion "latest"
+
+  targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("%{wks.location}/build/" .. outputdir .. "%{prj.name}")
+
+  IncludeDir["entt"] = "%{wks.location}/libs/entt/src"
+
+  includedirs {}
+
+  files {
+    "premake5.lua",
+    "src/**.hpp",
+    "src/**.h",
+  }
+
+  -- enables natvis extensions for visual studio
+  filter "toolset:msc"
+    files {
+      "natvis/**.natvis"
+    }
